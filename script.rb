d421e9925b93783ca0b3b748d508b6a6897c67cc
EHoldener <eholdener12@gmail.com>
2022-03-19 23:13:35 -0700

    trying to prevent bugs as i go, one major issue is trying to handle inputs from users being incorrect and recalling the functions breaking the functions that initially called those functions. Variables aren't passing correctly. Will research more and bug fix... monday? Tomorrow is a very busy day for me

--- script.rb
+++ script.rb
@@ -10,7 +10,9 @@ class Game
 
   def create_game
     @guesser = determine_guesser
-    puts @guesser
+    code = generate_code
+    puts 'The computer has generated a code! Time for you to guess it.'
+    get_guess(code)
   end
 
   private
@@ -21,18 +23,33 @@ class Game
   def determine_guesser
     puts 'Would you like to guess, or create the code for the computer to guess?'
     puts '1 for guess, 2 for code'
-    begin
-      decision = gets.to_i
-    rescue
+    decision = gets.to_i
+    unless decision.is_a? Integer
       puts "Looks like you didn't enter a number! Try again."
       determine_guesser
-    else
-      unless decision.between?(1, 2)
-        puts 'Please enter either one or two!'
-        determine_guesser
-      end
+    end
+    unless decision.between?(1, 2)
+      puts 'Please enter either one or two!'
+      determine_guesser
     end
     decision
+  end
+
+  def generate_code
+    if @guesser == 1
+      code = @computer.create_code
+    else
+      code = @player.create_code
+    end
+  end
+
+  def get_guess(code)
+    if @guesser == 1
+      guess = @player.guess(code)
+      puts "Guess = #{guess}"
+    else 
+      # code for the computer to guess (not currently implemented)
+    end
   end
 end
 
@@ -42,8 +59,15 @@ class Player
     # code
   end
 
-  def guess()
-    # code
+  def guess(code)
+    puts 'Enter a number 1-6'
+    num = gets
+    num = num.to_i
+    unless num.between?(1, 6)
+      puts 'Looks like you entered an invalid entry! Please try again'
+      game.get_guess(code)
+    end
+    num
   end
 
   def create_code()
@@ -57,8 +81,13 @@ class Computer
     # code
   end
 
+  # randomly creates code, with repeats (potentially add option for no repeats later)
   def create_code()
-    # code
+    code = Array.new(3)
+    code.each_with_index do |code_letter, idx|
+      code[idx] = rand(1..6)
+    end
+    code
   end
 end
 
